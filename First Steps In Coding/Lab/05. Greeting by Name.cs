1a0cee38dc0382037c26c05dda16fa479154926b
Ivan <87208452+PowerCell12@users.noreply.github.com>
2023-01-10 21:25:36 +0200

    Create 05. Greeting by Name.cs

---
+++ First Steps In Coding/Lab/05. Greeting by Name.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace HelloSoftuni
+{
+
+    class program
+    {
+        static void Main(string[] args)
+        {
+           string name = Console.ReadLine();
+           Console.WriteLine($"Hello, {name}!");
+        }
+    }
+}
