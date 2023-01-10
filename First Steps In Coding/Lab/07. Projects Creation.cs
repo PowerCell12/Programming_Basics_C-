a691f2ca08656661088597b6cd2482b5183978b5
Ivan <87208452+PowerCell12@users.noreply.github.com>
2023-01-10 21:26:59 +0200

    Create 07. Projects Creation.cs

---
+++ First Steps In Coding/Lab/07. Projects Creation.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace HelloSoftuni
+{
+
+    class program
+    {
+        static void Main(string[] args)
+        {
+        string name = Console.ReadLine();
+        int numbers_of_days = int.Parse(Console.ReadLine());
+        Console.WriteLine($"The architect {name} will need " + numbers_of_days * 3 + $" hours to complete {numbers_of_days} project/s.");
+        }
+    }
+}
