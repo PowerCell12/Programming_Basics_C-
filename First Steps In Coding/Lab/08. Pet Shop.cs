41b83093a573d7c314182a17ffe7fd862cb9b03d
Ivan <87208452+PowerCell12@users.noreply.github.com>
2023-01-14 13:08:21 +0200

    Update 08. Pet Shop.cs

--- First Steps In Coding/Lab/08. Pet Shop.cs
+++ First Steps In Coding/Lab/08. Pet Shop.cs
@@ -1,16 +1,7 @@
-using System;
+double number_of_dog_food = double.Parse(Console.ReadLine());
 
-namespace HelloSoftuni
-{
+double number_of_cat_food = double.Parse(Console.ReadLine());
 
-    class program
-    {
-        static void Main(string[] args)
-    {
-    double number_of_dog_food = double.Parse(Console.ReadLine());
-    double number_of_cat_food = double.Parse(Console.ReadLine());
-    double the_two = (number_of_dog_food * 2.5) + (number_of_cat_food * 4);
-    Console.WriteLine(the_two + " lv.");
-    }
- }
-}
+double the_two = (number_of_dog_food * 2.5) + (number_of_cat_food * 4);
+
+Console.WriteLine(the_two + " lv.");
