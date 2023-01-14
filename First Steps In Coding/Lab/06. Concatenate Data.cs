7d3610ea2f3c802b265699b268c1189d0c748670
Ivan <87208452+PowerCell12@users.noreply.github.com>
2023-01-14 13:11:26 +0200

    Update 06. Concatenate Data.cs

--- First Steps In Coding/Lab/06. Concatenate Data.cs
+++ First Steps In Coding/Lab/06. Concatenate Data.cs
@@ -1,17 +1,9 @@
-using System;
+string First_name = Console.ReadLine();
 
-namespace HelloSoftuni
-{
+string last_name  = Console.ReadLine(); 
 
-    class program
-    {
-        static void Main(string[] args)
-        {
-         string First_name = Console.ReadLine();
-         string last_name  = Console.ReadLine(); 
-         int age = int.Parse(Console.ReadLine());
-         string town = Console.ReadLine();
-         Console.WriteLine($"You are {First_name} {last_name}, a {age}-years old person from {town}.");
-        }
-    }
-}
+int age = int.Parse(Console.ReadLine());
+
+string town = Console.ReadLine();
+
+Console.WriteLine($"You are {First_name} {last_name}, a {age}-years old person from {town}.");
