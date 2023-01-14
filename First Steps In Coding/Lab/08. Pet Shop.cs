d18d816bfe4527144283bfcda3d50eb7a0e00b3d
Ivan <87208452+PowerCell12@users.noreply.github.com>
2023-01-14 13:06:16 +0200

    Create 08. Pet Shop.cs

---
+++ First Steps In Coding/Lab/08. Pet Shop.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace HelloSoftuni
+{
+
+    class program
+    {
+        static void Main(string[] args)
+    {
+    double number_of_dog_food = double.Parse(Console.ReadLine());
+    double number_of_cat_food = double.Parse(Console.ReadLine());
+    double the_two = (number_of_dog_food * 2.5) + (number_of_cat_food * 4);
+    Console.WriteLine(the_two + " lv.");
+    }
+ }
+}
