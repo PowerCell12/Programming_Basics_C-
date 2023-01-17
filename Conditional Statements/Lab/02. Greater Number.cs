cdcb77b188e7c61406a194c490fdb3c9b205161d
Ivan <87208452+PowerCell12@users.noreply.github.com>
2023-01-17 23:17:14 +0200

    Create 02. Greater Number.cs

---
+++ Conditional Statements/Lab/02. Greater Number.cs
@@ -0,0 +1,11 @@
+double first_number = double.Parse(Console.ReadLine());
+double second_number = double.Parse(Console.ReadLine());
+    
+if (first_number > second_number)
+{
+  Console.WriteLine(first_number);
+}
+else
+{
+  Console.WriteLine(second_number);
+}
