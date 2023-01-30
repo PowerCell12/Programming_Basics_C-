d634b233bf314f8745e5a7ff436fe5c39d359477
Ivan <87208452+PowerCell12@users.noreply.github.com>
2023-01-30 23:12:35 +0200

    Create 02.Weekend or Working Day.cs

---
+++ Conditional Statements Advanced/Lab/02.Weekend or Working Day.cs
@@ -0,0 +1,21 @@
+string day = Console.ReadLine();
+
+switch (day)
+{
+    case "Monday":
+    case "Tuesday":
+    case "Wednesday":
+    case "Thursday":
+    case "Friday":
+    Console.WriteLine("Working day");
+    break;
+
+    case "Sunday":
+    case "Saturday":
+    Console.WriteLine("Weekend");
+    break;
+
+    default:
+    Console.WriteLine("Error");
+    break;
+}
