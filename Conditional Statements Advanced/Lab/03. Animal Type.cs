ce8a2e5be4aebcdf5dd362277545f94f44c85f0e
Ivan <87208452+PowerCell12@users.noreply.github.com>
2023-01-30 23:13:04 +0200

    Create 03. Animal Type.cs

---
+++ Conditional Statements Advanced/Lab/03. Animal Type.cs
@@ -0,0 +1,18 @@
+string name = Console.ReadLine();
+
+switch (name)
+{
+    case "dog":
+    Console.WriteLine("mammal");
+    break;
+
+    case "crocodile":
+    case "tortoise":
+    case "snake":
+    Console.WriteLine("reptile");
+    break;
+
+    default:
+    Console.WriteLine("unknown");
+    break;
+}
