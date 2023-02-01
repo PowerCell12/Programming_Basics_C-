7f5e39e8051d8e5a6c2c429de250a2eac80cc5ab
Ivan <87208452+PowerCell12@users.noreply.github.com>
2023-02-01 23:33:54 +0200

    Create 09. Fruit or Vegetable.cs

---
+++ Conditional Statements Advanced/Lab/09. Fruit or Vegetable.cs
@@ -0,0 +1,27 @@
+string product = Console.ReadLine();
+string type = " ";
+
+switch (product)
+{
+    case "banana":
+    case "apple":
+    case "kiwi":
+    case "cherry":
+    case "lemon":
+    case "grapes":
+     type = "fruit";
+     break;
+
+    case "tomato":
+    case "cucumber":
+    case "pepper":
+    case "carrot":
+        type = "vegetable";
+        break;
+
+    default:
+        type = "unknown";
+        break;
+}
+
+Console.WriteLine(type);
