b03fd52911f06ede786b82a4467abd2471e2ec66
Ivan <87208452+PowerCell12@users.noreply.github.com>
2023-02-02 21:15:15 +0200

    Create 02. Summer Outfit.cs

---
+++ Conditional Statements Advanced/Exercise/02. Summer Outfit.cs
@@ -0,0 +1,65 @@
+int degrees = int.Parse(Console.ReadLine());
+string time = Console.ReadLine();
+string outfit = "";
+string shoes = "";
+
+if (time == "Morning")
+{
+    if (10 <= degrees && degrees <= 18)
+    {
+        outfit = "Sweatshirt";
+        shoes = "Sneakers";
+    }
+    else if (18 < degrees && degrees <= 24)
+    {
+        outfit = "Shirt";
+        shoes = "Moccasins";
+    }
+    else if (degrees >= 25)
+    {
+        outfit = "T-Shirt";
+        shoes = "Sandals";
+    }
+}
+
+if (time == "Afternoon")
+{
+    if (10 <= degrees && degrees <= 18)
+    {
+        outfit = "Shirt";
+        shoes = "Moccasins";
+    }
+    else if (18 < degrees && degrees <= 24)
+    {
+        outfit = "T-Shirt";
+        shoes = "Sandals";
+    }
+    else if (degrees >= 25)
+    {
+        outfit = "Swim Suit";
+        shoes = "Barefoot";
+    }
+}
+
+
+if (time == "Evening")
+{
+    if (10 <= degrees && degrees <= 18)
+    {
+        outfit = "Shirt";
+        shoes = "Moccasins";
+    }
+    else if (18 < degrees && degrees <= 24)
+    {
+        outfit = "Shirt";
+        shoes = "Moccasins";
+    }
+    else if (degrees >= 25)
+        {
+            outfit = "Shirt";
+            shoes = "Moccasins";
+        }
+}
+
+
+Console.WriteLine($"It's {degrees} degrees, get your {outfit} and {shoes}.");
